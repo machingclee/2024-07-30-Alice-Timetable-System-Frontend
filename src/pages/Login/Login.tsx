3af04e4babcc04d9e90febe00e9e8250a5b59ec2
machingclee <machingclee@gmail.com>
2025-04-14 04:48:41 +0800

    separate two clients, one for logined, and one for unlogined.

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -16,6 +16,7 @@ export default function Login() {
     const [password, setPassword] = useState('');
 
     const login = async () => {
+        console.log('login with', email, password);
         await dispatch(
             AuthThunkAction.userLogin({
                 email,
