223acf811a5ab8cdbf6c9b8bc41f4362b05d57c3
machingclee <machingclee@gmail.com>
2025-07-08 11:24:31 +0800

    add mutation

--- src/utils/normalizeUtil.ts
+++ src/utils/normalizeUtil.ts
@@ -1,7 +1,13 @@
 /* eslint-disable-file */
 import { normalize as normalize_, schema } from 'normalizr';
 
-const normalize = <T, ID = string>({ targetArr, idAttribute }: { targetArr: T[]; idAttribute: string }) => {
+const normalize = <T, ID = string>({
+    targetArr,
+    idAttribute,
+}: {
+    targetArr: T[];
+    idAttribute: string | ((value: T) => string);
+}) => {
     const objectEntity = new schema.Entity<Selection>('object', undefined, {
         idAttribute,
     });
